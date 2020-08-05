3381b8fed1bbb068e4b357487c1a178c2eb2e193
yangjingliu <yangjingliu@izuche.com>
2020-08-05 20:24:24 +0800

    脚本更新

--- deploy.sh
+++ deploy.sh
@@ -7,39 +7,22 @@
 
 # 输入你的环境上tomcat的全路径
 # export TOMCAT_APP_PATH=tomcat在部署机器上的路径
-
-### base 函数
-killTomcat()
-{
-    pid=`ps -ef|grep tomcat|grep java|awk '{print $2}'`
-    echo "tomcat Id list :$pid"
-    if [ "$pid" = "" ]
-    then
-      echo "no tomcat pid alive"
-    else
-      kill -9 $pid
-    fi
-}
 cd $PROJ_PATH/order
 mvn clean install
 
 # 停tomcat
-killTomcat
+docker stop tomcat_order
 
 # 删除原有工程
-rm -rf $TOMCAT_APP_PATH/webapps/ROOT
-rm -f $TOMCAT_APP_PATH/webapps/ROOT.war
-rm -f $TOMCAT_APP_PATH/webapps/order.war
+rm -rf $TOMCAT_APP_PATH/ROOT
+rm -f $TOMCAT_APP_PATH/ROOT.war
+rm -f $TOMCAT_APP_PATH/order.war
 
 # 复制新的工程
-cp $PROJ_PATH/order/target/order.war $TOMCAT_APP_PATH/webapps/
-
-cd $TOMCAT_APP_PATH/webapps/
-mv order.war ROOT.war
+cp $PROJ_PATH/order/target/order.war $TOMCAT_APP_PATH/ROOT.war
 
 # 启动Tomcat
-cd $TOMCAT_APP_PATH/
-sh bin/startup.sh
+docker start tomcat_order
 
 
 
